490019baa29237d52b9023b44a3bef78fbb4d50d
kadin <kadinalone@gmail.com>
2025-04-18 20:19:58 -0600

    finished 3d graph

--- examples/graph3d.glsl
+++ examples/graph3d.glsl
@@ -1,14 +1,18 @@
 float f(vec2 pos) {
-  return sin(pos.x) + sin(pos.y);
+  return sin(pos.x)/cos(pos.y);
 }
 
 float g(vec2 pos) {
-  return 1.0/sin(pos.x);
+  return cos(length(pos));
 }
 
 // tool functions
 vec3 voxCol(vec3 pos) {
-  return sin((pos / 4.0 + time) / 2.0) / 2.0 + 0.5;
+  return sin((pos / 2.0 + time) / 2.0) / 2.0 + 0.5;
+}
+
+vec3 voxCol(vec3 pos, vec3 col) {
+  return (sin((pos / 2.0 + time) / 2.0) / 2.0 + 0.5) * col;
 }
 
 // main functions
@@ -17,12 +21,16 @@ vec4 getSkybox(vec3 dir) {
 }
 
 vec4 getVoxel(vec3 pos) {
-  // if (pos.z >= 32.0) return vec4(sin(pos.x / 4.0 - time) / 2.0 + 0.5, float(floor(pos.y) != 0.0), float(floor(pos.x) != 0.0), 1.0); // back wall
   vec3 scale = vec3(16.0);
   // f(x)
-  if (abs(f(pos.xz / scale.xz) * scale.y - pos.y) < 1.0) return vec4(voxCol(pos), 1.0);
+  if (abs(f(pos.xz / scale.xz) * scale.y - pos.y) < 1.0) return vec4(voxCol(pos, vec3(0.0, 1.0, 0.0)) + vec3(1.0, 0.0, 0.0), 1.0);
   // g(x)
-  if (abs(g(pos.xz / scale.xz) * scale.y - pos.y) < 1.0) return vec4(voxCol(pos), 1.0);
+  if (abs(g(pos.xz / scale.xz) * scale.y - pos.y) < 1.0) return vec4(voxCol(pos, vec3(1.0, 0.0, 0.0)) + vec3(0.0, -1.0, 1.0), 1.0);
+  //* colored axis (remove second slash before * to disable, improving perforrmance)
+  if (abs(pos.y) < 0.1 && abs(pos.z) < 0.1) return vec4(voxCol(pos, vec3(1.0, 0.0, 0.0)), 1.0); // x axis
+  if (abs(pos.x) < 0.1 && abs(pos.z) < 0.1) return vec4(voxCol(pos, vec3(0.0, 1.0, 0.0)), 1.0); // y axis
+  if (abs(pos.x) < 0.1 && abs(pos.y) < 0.1) return vec4(voxCol(pos, vec3(0.0, 0.0, 1.0)), 1.0); // Z axis
+  //*/
   // miss case
   return vec4(1.0, 1.0, 1.0, 0.0);
 }
